d23f45b6451928e88caa3b0b37611c8484f17021
Tanmay <tanmay.jain@particle41.com>
2023-08-01 10:29:36 +0530

    added unit test for cabin seating

--- src/Invoice.ts
+++ src/Invoice.ts
@@ -1,25 +1,39 @@
 const OpenSeatPrice: number = 5000;
 const OpenSeatGST: number = 1.18;
+const cabinSeatPrice: number = 10000;
+const CabinSeatGST: number = 1.18;
 export class Invoice {
     constructor() {
     }
 
 
-    print(openSeat: number): number {
+    print(openSeat: number, cabinSeat: number): number {
         
         const openSeatCost = this.calculateOpenSeatCost(openSeat);
-        const openSeatCostWithGST = this.calculateGST(openSeatCost);
+        const cabinSeatCost = this.calculateCabinSeatCost(cabinSeat);
+        const openSeatCostWithGST = this.calculateOpenSeatGST(openSeatCost);
+        const cabinSeatCostWithGST = this.calculateCabinSeatGST(cabinSeatCost);
         
-        return openSeatCostWithGST;
+        return openSeatCostWithGST+cabinSeatCostWithGST;
     }
 
-    private calculateGST(openSeatCost: number) {
+    private calculateOpenSeatGST(openSeatCost: number) {
         const totalOpenSeatCost = openSeatCost*OpenSeatGST;
         return totalOpenSeatCost;
     }
 
     private calculateOpenSeatCost(openSeat: number): number  {
         const cost = openSeat*OpenSeatPrice;
+        return cost;
+    }
+
+    private calculateCabinSeatGST(cabinSeatCost: number) {
+        const totalOpenSeatCost = cabinSeatCost*CabinSeatGST;
+        return totalOpenSeatCost;
+    }
+
+    private calculateCabinSeatCost(cabinSeat: number): number  {
+        const cost = cabinSeat*cabinSeatPrice;
         return cost;
     }
 }
--- tests/Invoice.spec.ts
+++ tests/Invoice.spec.ts
@@ -3,6 +3,13 @@ import { Invoice } from "../src/Invoice";
 describe("Invoice", () => {
     test("should print invoice as 11800 when only 2 open seats are booked", () => {
         const openSeat: number = 2;
-        expect(new Invoice().print(openSeat)).toEqual(11800);
+        const cabinSeat: number = 0;
+        expect(new Invoice().print(openSeat, cabinSeat)).toEqual(11800);
+    });
+
+    test("should print invoice as 35400 when only 3 cabin seats are booked", () => {
+        const openSeat: number = 0;
+        const cabinSeat: number = 3;
+        expect(new Invoice().print(openSeat, cabinSeat)).toEqual(35400);
     });
 });
