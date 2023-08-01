a8513ef136a1b83b2291a2bf8cddef3acb451473
Tanmay <tanmay.jain@particle41.com>
2023-08-01 10:54:05 +0530

    added unit test for conference Room booking

--- src/Invoice.ts
+++ src/Invoice.ts
@@ -2,19 +2,43 @@ const OpenSeatPrice: number = 5000;
 const OpenSeatGST: number = 1.18;
 const cabinSeatPrice: number = 10000;
 const CabinSeatGST: number = 1.18;
+const OpenSeatConferenceRoomHour: number = 5;
+const CabinSeatConferenceRoomHour: number = 10;
+const ConferenceRoomCost: number = 200;
+const conferenceRoomGST: number = 1.18;
 export class Invoice {
     constructor() {
     }
 
 
-    print(openSeat: number, cabinSeat: number): number {
+    print(openSeat: number, cabinSeat: number, conferenceRoomHours: number): number {
         
         const openSeatCost = this.calculateOpenSeatCost(openSeat);
         const cabinSeatCost = this.calculateCabinSeatCost(cabinSeat);
         const openSeatCostWithGST = this.calculateOpenSeatGST(openSeatCost);
         const cabinSeatCostWithGST = this.calculateCabinSeatGST(cabinSeatCost);
+        const conferenceRoomCost = this.calculateConferenceRoomCost(openSeat, cabinSeat, conferenceRoomHours);
+        const conferenceRoomCostWithGST = this.calculateConferenceRoomGST(conferenceRoomCost);
+
+        return openSeatCostWithGST+cabinSeatCostWithGST+conferenceRoomCostWithGST;
+    }
+    
+    calculateConferenceRoomGST(conferenceRoomCost: number): number {
+        const totalOpenSeatCost = conferenceRoomCost*conferenceRoomGST;
+        return totalOpenSeatCost;
+    }
+
+    calculateConferenceRoomCost(openSeat: number, cabinSeat: number, conferenceRoomHours: number): number {
         
-        return openSeatCostWithGST+cabinSeatCostWithGST;
+        const freeOpenSeatHour = openSeat*OpenSeatConferenceRoomHour;
+        const freeCabinSeatHour = cabinSeat*CabinSeatConferenceRoomHour;
+        const unpaidConferenceRoomHour = conferenceRoomHours - freeOpenSeatHour - freeCabinSeatHour
+        if(unpaidConferenceRoomHour > 0){
+            return unpaidConferenceRoomHour*ConferenceRoomCost;
+        }
+        else{
+            return 0;
+        }
     }
 
     private calculateOpenSeatGST(openSeatCost: number) {
--- tests/Invoice.spec.ts
+++ tests/Invoice.spec.ts
@@ -4,12 +4,28 @@ describe("Invoice", () => {
     test("should print invoice as 11800 when only 2 open seats are booked", () => {
         const openSeat: number = 2;
         const cabinSeat: number = 0;
-        expect(new Invoice().print(openSeat, cabinSeat)).toEqual(11800);
+        const conferenceRoomHours: number = 0;
+        expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours)).toEqual(11800);
     });
 
     test("should print invoice as 35400 when only 3 cabin seats are booked", () => {
         const openSeat: number = 0;
         const cabinSeat: number = 3;
-        expect(new Invoice().print(openSeat, cabinSeat)).toEqual(35400);
+        const conferenceRoomHours: number = 0;
+        expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours)).toEqual(35400);
+    });
+
+    test("should print invoice as 47200 when 2 open seats, 3 cabin seats are booked and 35 hours of conference room is booked", () => {
+        const openSeat: number = 2;
+        const cabinSeat: number = 3;
+        const conferenceRoomHours: number = 35;
+        expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours)).toEqual(47200);
+    });
+
+    test("should print invoice as 47200 when 2 open seats, 3 cabin seats are booked and 35 hours of conference room is booked", () => {
+        const openSeat: number = 2;
+        const cabinSeat: number = 3;
+        const conferenceRoomHours: number = 50;
+        expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours)).toEqual(49560);
     });
 });
