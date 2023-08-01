6c0b954a3cf40d812cc43e58d2b34a8625501ed6
Tanmay <tanmay.jain@particle41.com>
2023-08-01 11:16:15 +0530

    added few more test case accoring to requirments.

--- tests/Invoice.spec.ts
+++ tests/Invoice.spec.ts
@@ -40,4 +40,28 @@ describe("Invoice", () => {
         const meals: number = 5;
         expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours,meals)).toEqual(47760);
     });
+
+    test("should print invoice as 47760 when 2 open seats, 3 cabin seats are booked, 35 hours of conference room and 5 meals is booked", () => {
+        const openSeat: number = 2;
+        const cabinSeat: number = 3;
+        const conferenceRoomHours: number = 35;
+        const meals: number = 5;
+        expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours,meals)).toEqual(47760);
+    });
+
+    test("should print invoice as 47760 when 0 open seats, 1 cabin seats are booked, 50 hours of conference room and 10 meals is booked", () => {
+        const openSeat: number = 0;
+        const cabinSeat: number = 1;
+        const conferenceRoomHours: number = 50;
+        const meals: number = 10;
+        expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours,meals)).toEqual(22360);
+    });
+
+    test("should print invoice as 47760 when 2 open seats, 0 cabin seats are booked, 0 hours of conference room and 30 meals is booked", () => {
+        const openSeat: number = 2;
+        const cabinSeat: number = 0;
+        const conferenceRoomHours: number = 0;
+        const meals: number = 30;
+        expect(new Invoice().print(openSeat, cabinSeat, conferenceRoomHours,meals)).toEqual(15160);
+    });
 });
